8ad8ebb0b8692092803e795dc8e37bb2f34df145
tatsuya.shimizu <t.shimizu1115@gmail.com>
2024-08-22 17:15:23 +0900

    feat: postsのルーティングを追加する

--- src/main.tsx
+++ src/main.tsx
@@ -16,10 +16,12 @@ const rootRoute = createRootRoute({
   component: () => (
     <>
       <div>
-        <Link to="/">index</Link>|<Link to="/about">about</Link>
+        <Link to="/">index</Link>|<Link to="/about">about</Link>|
+        <Link to="/posts">posts</Link>
       </div>
       <hr />
       <Outlet />
+      {/* developmentだけ出す仕組みがたぶん必要 */}
       <TanStackRouterDevtools />
     </>
   ),
@@ -41,8 +43,47 @@ const aboutRoute = createRoute({
   },
 });
 
-// TODO: Tree作る意味とか意図とか
-const routeTree = rootRoute.addChildren([indexRoute, aboutRoute]);
+const postsRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "posts",
+});
+
+const postsIndexRoute = createRoute({
+  getParentRoute: () => postsRoute,
+  // Notice the single slash `/` here
+  path: "/",
+  component: () => {
+    return (
+      <div>
+        <Link to="/posts/$postId" params={{ postId: "1" }}>
+          1
+        </Link>
+        ,
+        <Link to="/posts/$postId" params={{ postId: "2" }}>
+          2
+        </Link>
+      </div>
+    );
+  },
+});
+
+const postIdRoute = createRoute({
+  getParentRoute: () => postsRoute,
+  path: "$postId",
+  // In a loader
+  // loader: ({ params }) => fetchPost(params.postId),
+  // Or in a component
+  component: () => {
+    const { postId } = postIdRoute.useParams();
+    return <div>Post ID: {postId}</div>;
+  },
+});
+
+const routeTree = rootRoute.addChildren([
+  indexRoute,
+  aboutRoute,
+  postsRoute.addChildren([postsIndexRoute, postIdRoute]),
+]);
 
 const router = createRouter({ routeTree });
 
